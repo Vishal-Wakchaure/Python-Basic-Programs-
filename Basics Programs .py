79ed24ba97196cc35abccf7234afd1af857b1ba3
Vishal Wakchaure <wakchaurev00@gmail.com>
2026-02-18 22:49:06 +0530

    basic program

---
+++ Basics Programs .py
@@ -0,0 +1,30 @@
+# Name = input("Enter Name")
+# age = int (input("Enter Age "))
+# marks = float(input("Enter Marks"))
+
+# print("Welcome",Name)
+# print("age=",age)
+# print("marks=",marks)
+
+
+# Sum of two Numbers
+# a = int(input("Enter Any number"))
+# b = int(input("Enter any Number"))
+
+# print("Sum is ",a+b)
+
+# Area of Square 
+# a= int(input("Enter any Number"))
+# print("Area of square :",a*a)
+
+# Average calculator
+
+# a=float(input("Enter first number"))
+# b=float(input("Enter second number"))
+
+# print("Average is:", (a+b)/2)
+
+# True False 
+a= int(input("Enter first number"))
+b = int(input("enter second number"))
+print(a>=b)

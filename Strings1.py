21de74a790ea0ed65749418d87c425a399d5ec0b
Vishal Wakchaure <wakchaurev00@gmail.com>
2026-02-19 09:48:24 +0530

    Python programs

---
+++ Strings1.py
@@ -0,0 +1,33 @@
+# str1 = " This is a string. \n We are creating it in python"
+# print(str1)
+
+#Concatenation 
+# str1 = "Vishal "
+# str2 = "wakchaure"
+# final_str = str1+str2
+# print(final_str)
+
+# print(len(str1))
+# print(len(str2))
+
+# index = str1[-3:-1]
+# print(index)
+
+# String Functions 
+# print(str1.endswith("hal"))
+# str2= print(str2.capitalize())
+# str = (str1.find("vis"))
+# print(str)
+
+# WAP to input user’s first name & print its length.
+# name = input("Enter user name ")
+# print(name)
+# length = len(name)
+# print("Length of name is ",length)
+
+# WAP to find the occurrence of ‘$’ in a String.
+# name = input("Enter user name ")
+# print(name)
+
+str = "Hi, $I am $ Symbol $  "
+print(str.count("$"))

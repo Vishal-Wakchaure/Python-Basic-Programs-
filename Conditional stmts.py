bf6a0016aaaae0dbe0d1f340cb6d04105a3bf5f5
Vishal Wakchaure <wakchaurev00@gmail.com>
2026-02-19 12:05:51 +0530

    conditional statements in python

---
+++ Conditional stmts.py
@@ -0,0 +1,56 @@
+# age = 21
+
+# if(age>=18):
+#     print("can vote  ")
+#     print("can Drive ")
+
+# light = "Green"
+
+# if(light=="red"):
+#     print("Stop")
+# elif(light=="Green"):
+#     print("Go")# Indentation(Proper spacing)
+# elif(light=="Yellow"):
+#     print("Wait")int
+
+# marks = int(input("Enter Marks"))
+
+# if(marks>=90):
+#     grade = "A"
+# elif(marks>=80 and marks<90):
+#     grade = "B"
+# elif(marks >=70 and marks<80):
+#     grade = "C"
+# else:
+#     grade = "D"
+# print("Grade of the student -> ",grade)
+
+# To check number is odd or even 
+
+# n = int(input("Enter Any Number"))
+# if(n!=0):
+#     print("Number is even")
+# else:
+#     print("Number is odd ")
+
+# # Largest Number among three/four  Numbers
+# a = int(input("Enter first number"))
+# b = int(input("Enter second Number"))
+# c = int(input("Enter Third Number"))
+# d= int(input("Enter fourth Number"))
+# if(a>=b and a>=c ):
+#     print(a," is greater ")
+# elif(b>=c and b>=d):
+#     print(b," is greater")
+# elif(c>=d):
+#     print(c," is greater")
+# else: 
+#     print(d,"is greater")
+
+# Number is Multiple of 7 or not 
+
+n = int(input("Enter any number"))
+if(n%7==0):
+    print("Number is multiple of 7 ")
+else:
+    print("Number is not multiple by 7")

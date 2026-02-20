a32d9f80aa95349e933fab2e5d8bef230e1ce927
Vishal Wakchaure <wakchaurev00@gmail.com>
2026-02-20 23:06:37 +0530

    Updated python programs

---
+++ ListTuples.py
@@ -0,0 +1,61 @@
+# marks = [94.4,89.7,90.5,70.5,79.7]
+# print(marks)
+# print(type(marks))
+# print(len(marks))
+
+# student = ["karan",98.4,7,"Delhi"]
+# print(student[0])
+# student[0] = "Arjun" 
+# print(student[0])
+# print(student[3])
+# student[3] = "Pune"
+# print(student[3])
+
+# Tuples 
+# tuple1 = (87,65,20,30,40)
+# print(tuple1[0])
+# print(tuple1.index(20))
+
+# WAP to ask the user to enter names of their 3 favorite movies & store them in a list
+
+# WAP to ask the user to enter names of their 3 favorite movies
+# and store them in a list
+
+# movies = []
+
+# for i in range(3):
+#     name = input("Enter your favorite movie name: ")
+#     movies.append(name)
+
+# print("Your favorite movies are:", movies)
+# WAP to ask the user to enter names of their 3 favorite movies
+# without using loop
+
+# movie1 = input("Enter your 1st favorite movie: ")
+# movie2 = input("Enter your 2nd favorite movie: ")
+# movie3 = input("Enter your 3rd favorite movie: ")
+
+# movies = [movie1, movie2, movie3]
+
+# print("Your favorite movies are:", movies)
+
+# WAP to count the number of students with the "A" grade in the tuple
+
+# WAP to count the number of students with the “A” grade in the following tuple.
+# [”C”, “D”, “A”, “A”, “B”, “B”, “A”
+ 
+# grades = ("C", "D", "A", "A", "B", "B", "A")
+
+# count_A = grades.count("A")
+
+# print("Number of students with A grade:", count_A)
+
+# Store the above values in a list & sort them from “A” to “D”.
+
+# Store the given values in a list and sort from "A" to "D"
+
+grades_list = ["C", "D", "A", "A", "B", "B", "A"]
+
+grades_list.sort()
+
+print("Sorted grades:", grades_list)
---
+++ Loops.py
@@ -0,0 +1,8 @@
+# Using For Loop 
+for i in range (1,11):
+    print(i)
+# Using While loop 
+i=1 
+while i<=10 :
+    print(i)
+    i=i+1
